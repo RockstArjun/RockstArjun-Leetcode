4103efedb8bad2e8f7025949db609bb270509aa4
RockstArjun <chauhanarjun064@gmail.com>
2023-02-17 22:45:32 +0530

    solved

---
+++ Sorting/2160. Minimum Sum of Four Digit Number After Splitting Digits/2160. Minimum Sum of Four Digit Number After Splitting Digits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+        vector<int> arjun;
+        int result;
+       while(num>0){
+           arjun.push_back(num%10);
+           num/=10;
+       } 
+       sort(arjun.begin(),arjun.end());
+        result= (((arjun[0]*10)+arjun[3]) + ((arjun[1]*10)+arjun[2]));
+        return result;
+    }
+};
